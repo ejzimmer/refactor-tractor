53b7c9fb54418b7742b4ba0fdb37520543db3057
Erin Zimmer <ejzimmer@gmail.com>
2021-08-02 12:19:54 +1000

    example 2

---
+++ examples/example_2.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import format from 'date-fns/format'
+import { formatShortDate } from './example_1'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+
+function DayOfTheWeekWidget({ date = '' }: { date?: string | number }) {
+  if (!date) {
+    return <div>No date entered</div>
+  }
+
+  const formattedDate = formatShortDate(date)
+  const dayOfTheWeek = format(new Date(date), 'EEEE')
+
+  return <div>{formattedDate} is a {dayOfTheWeek}</div>
+}
+
+describe('DayOfTheWeekWidget', () => {
+})
